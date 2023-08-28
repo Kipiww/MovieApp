f3a7f5da10773007dda1d8641106149642a19a80
Kipiww <130513312+Kipiww@users.noreply.github.com>
2023-08-28 15:08:44 +0800

    Add more functionalities to the web

--- script.js
+++ script.js
@@ -2,3 +2,51 @@
 const input = document.getElementById('inputField');
 const searchBtn = document.getElementById('search');
 const result = document.querySelector('.resultField');
+
+
+
+
+
+searchBtn.addEventListener('click',() => {
+    
+    const movieName = input.value;
+    const apiKey = `713fbff6`;  
+    const apiUrl = `http://www.omdbapi.com/?t=${movieName}&apikey=${apiKey}`;
+   
+
+    //if moviename  has no value
+    if (movieName.length <= 0 || !movieName) {
+        alert(`Please type a Movie`)
+    } else {
+        result.innerHTML = '';
+       fetch(apiUrl).then(respo => respo.json()).then(data => {
+        console.log(data)
+       
+             result.innerHTML = ` <img src=${data.Poster}>    
+            <div class="title">
+                <h1>${data.Title}</h1>
+                <div class="ratings">
+                    <i class="fa-solid fa-star"></i>
+                    <p>${data.imdbRating}</p>
+                </div>    
+                <div class="date">
+                    <p>${data.Rated}</p>
+                    <p>${data.Year}</p>
+                    <p>${data.Runtime}</p>
+                </div>
+                <div class="genre">
+                    <p>${data.Genre.split(",").join("</p><p>")}</p>
+                    <p>Crime</p>
+                    <p>Drama</p>
+                </div>
+            </div>
+           
+        </div>
+       `
+       })
+    }
+    
+
+
+})
+
