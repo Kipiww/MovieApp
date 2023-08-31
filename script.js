d500067ae146e3666f4aa692fb79c5602a68164f
Kipiww <130513312+Kipiww@users.noreply.github.com>
2023-08-31 16:07:09 +0800

    commit

--- script.js
+++ script.js
@@ -3,10 +3,6 @@ const input = document.getElementById('inputField');
 const searchBtn = document.getElementById('search');
 const result = document.querySelector('.resultField');
 
-
-
-
-
 searchBtn.addEventListener('click',() => {
     
     const movieName = input.value;
@@ -18,35 +14,41 @@ searchBtn.addEventListener('click',() => {
     if (movieName.length <= 0 || !movieName) {
         alert(`Please type a Movie`)
     } else {
-        result.innerHTML = '';
+        result.innerHTML = '<p>Searching...</p>';
        fetch(apiUrl).then(respo => respo.json()).then(data => {
-        console.log(data)
+
+             result.innerHTML =  `<div class="resultField">
+             <div class="info">
+               <img src=${data.Poster} class="imgDef">
+               <div class="title">
+                   <h1>${data.Title}</h1>
+                   <div class="ratings">
+                       <i class="fa-solid fa-star"></i>
+                       <p>${data.imdbRating}</p>
+                   </div>
+                       <div class="date">
+                           <p>${data.Rated}</p>
+                           <p>${data.Year}</p>
+                           <p>${data.Runtime}</p>
+                       </div>
+                       <div class="genre">
+                           <p>${data.Genre.split(",").join("</p><p>")}</p>
+                       </div>
+                   </div>
+               </div> 
+   
+             <div class="moreInfo">
+               <div class="plot">
+                   <h3>Plot:</h3>
+                   <p>${data.Plot}</p>
+               </div>
+               <div class="cast">
+                   <h3>Cast:</h3>
+                   <p>${data.Actors}</p>
+               </div>
+           </div>`
        
-             result.innerHTML = ` <img src=${data.Poster}>    
-            <div class="title">
-                <h1>${data.Title}</h1>
-                <div class="ratings">
-                    <i class="fa-solid fa-star"></i>
-                    <p>${data.imdbRating}</p>
-                </div>    
-                <div class="date">
-                    <p>${data.Rated}</p>
-                    <p>${data.Year}</p>
-                    <p>${data.Runtime}</p>
-                </div>
-                <div class="genre">
-                    <p>${data.Genre.split(",").join("</p><p>")}</p>
-                    <p>Crime</p>
-                    <p>Drama</p>
-                </div>
-            </div>
-           
-        </div>
-       `
        })
     }
-    
-
-
 })
 
